997ab3d443ee5f81e67cbc7ceb160756b85f339c
Harshit Yadav <2105891@kiit.ac.in>
2024-06-15 12:28:00 +0530

    Final commit and deployed 1. Updated package.json 2. fixed bugs in index.ts

--- index.ts
+++ index.ts
@@ -15,53 +15,50 @@ const app = new Hono();
 
 const cache = new Map();
 
-// creating class instance
+// Creating class instance
 class RedisRateLimiter {
-  static Instance: Ratelimit;
-  static getInstance(c: Context<Env, "/todos/:id", BlankInput>) {
-    // following singelton pattern
+  static Instance: Ratelimit | null = null;
+  static getInstance(c: Context<Env, "/todos/:id", BlankInput>): Ratelimit {
+    // Following singleton pattern
     if (!this.Instance) {
       const { UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN } = env<{
         UPSTASH_REDIS_REST_URL: string;
         UPSTASH_REDIS_REST_TOKEN: string;
       }>(c);
 
-      // access the database
+      // Access the database
       const redisClient = new Redis({
         token: UPSTASH_REDIS_REST_TOKEN,
         url: UPSTASH_REDIS_REST_URL,
       });
 
-      const rateLimit = new Ratelimit({
+      this.Instance = new Ratelimit({
         redis: redisClient,
-        limiter: Ratelimit.slidingWindow(10, "10 s"), // sliding window algorithm will be used to do the rate limiting
+        limiter: Ratelimit.slidingWindow(10, "10 s"), // Sliding window algorithm will be used to do the rate limiting
         ephemeralCache: cache,
       });
-    } else {
-      return this.Instance;
     }
+    return this.Instance;
   }
 }
 
-// middleware to set the rateLimiter in the context
+// Middleware to set the rateLimiter in the context
 app.use(async (c, next) => {
   const ratelimit = RedisRateLimiter.getInstance(c);
-  if (ratelimit) {
-    c.set("ratelimit", ratelimit);
-  }
+  c.set("ratelimit", ratelimit);
   await next();
 });
 
 app.get("/todos/:id", async (c) => {
   const ratelimit = c.get("ratelimit");
-  const ip = c.req.raw.headers.get("cf-connecting-ip");
-  const { success } = await ratelimit.limit(ip ?? "anonymous");
+  const ip = c.req.raw.headers.get("cf-connecting-ip") || "anonymous";
+  const { success } = await ratelimit.limit(ip);
 
   if (success) {
     const todoId = c.req.param("id");
     const todoIndex = Number(todoId);
     const todo = todos[todoIndex] || {};
-    return c.json({ todos: [] });
+    return c.json({ todo });
   } else {
     return c.json({ message: "Rate limit exceeded" }, { status: 429 });
   }
